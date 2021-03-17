66ad31d7709a0fde98fe92121fa0bf8564a93416
fepacheco20 <72778767+fepacheco20@users.noreply.github.com>
2021-03-17 16:21:52 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+// ****************************
+// To-do list
+// by Fernanda Pacheco 
+// ****************************
+
+// ***********INDEX************
+// 1- SELECTORS
+// 2- NEW LIST ITEM
+// 3- COMMANDS
+// ****************************
+
+
+// ***************************
+// 1 - SELECTORS
+// ***************************
 const list = document.querySelector("#todo-list");
 const addBtn = document.querySelector("#a-btn");
 const name = document.querySelector("#name");
 
+// ***************************
+// 2 - NEW LIST ITEM
+// ***************************
+
+// ADD BUTTON
 addBtn.addEventListener("click", function (e) {
     e.preventDefault();
     newItem();
@@ -18,7 +38,7 @@ const newItem = () => {
     todoText.contentEditable = "true";
 
     todoText.classList.add("item");
-    listItem.classList.add("todo-default", "list-container");
+    listItem.classList.add("todo-default", "list-line");
     deleteBtn.classList.add("far", "fa-trash-alt", "del-btn", "fa-icon");
 
     group.append(input, todoText)
@@ -28,21 +48,47 @@ const newItem = () => {
     todoText.focus();
 }
 
+// CTRL + I 
+// font: https://www.gavsblog.com/blog/detect-single-and-multiple-keypress-events-javascript
+let keysPressed = {};
+
+window.addEventListener('keydown', (e) => {
+    keysPressed[e.key] = true;
+    // console.log(keysPressed)
+
+    if (keysPressed['Control'] && e.key == 'i') {
+        // console.log(e.key);
+        newItem();
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    delete keysPressed[e.key];
+});
+
+// ENTER
+list.addEventListener('keydown', function (e) {
+    // console.log(e);
+    if (e.keyCode === 13 && e.srcElement.nodeName === "SPAN") {
+        e.preventDefault();
+        newItem();
+    }
+})
+
+// ***************************
+// 3 - COMMANDS
+// ***************************
+
+// 3.1 - TASK COMPLETE 
+// 3.2 - DELETE LIST ITEM
+// 3.3 - FOCUS 
 list.addEventListener("click", function (e) {
-    // console.dir(e.target);
     if (e.target.nodeName === "INPUT") {
-        e.target.parentElement.classList.toggle("todo-complete");
+        e.target.nextElementSibling.classList.toggle("strikeout");
     } else if (e.target.nodeName === "BUTTON") {
         e.target.parentElement.remove();
     } else if (e.target.nodeName === "LI") {
         e.target.children[0].children[1].focus();
-    }
-})
-
-window.addEventListener('keydown', function (e) {
-    if (e.code === "Enter") {
-        e.preventDefault();
-        newItem();
     }
 })
 
